577fd77476328020e422474739edf2936c6f89c3
Tanay Kothari <tanaykothari1998@gmail.cm>
2018-05-10 03:47:28 -0700

    Updated soldity and First version of webapp,
    Added OpenZepellin, inserted comments in OceanFund Solidity and rewrote
    the extract function

--- contracts/OceanFund.sol
+++ contracts/OceanFund.sol
@@ -1,81 +1,7 @@
 pragma solidity ^0.4.0;
 
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract OceanFund is Ownable {
     
@@ -89,11 +15,20 @@ contract OceanFund is Ownable {
     bool active = true;
     uint BROKER_FEE = 0.001 ether;
     uint POOL_FEE = 1; // * 0.01 
+    uint MAX_INVESTORS = 300;
+
     
-    function OceanFund() public {
+    function OceanFund(uint poolFee, uint maxInvestors) public {
         broker = msg.sender;
+        POOL_FEE = poolFee;
+        MAX_INVESTORS = maxInvestors;
     }
     
+    /* 
+      In case of an emergency scenario or a deadlock, the contract liquidates everyones investment
+      and allows them to withdraw it. 
+      Can only be called by the owner of the contract. 
+    */
     function close() public onlyOwner {
         active = false;
         
@@ -108,11 +43,20 @@ contract OceanFund is Ownable {
         totalInvestment = 0;
     }
     
-
-    function calcMaxInvestment() private returns (uint) {
+    /* 
+      Calculates the maximum amount you can have invest at this point in time. You can't invest
+      more money than the pool can pay you back on, since that would put the pool in deadlock
+      Note: There might be more subtleties to this, this is a hacky version to take care of this 
+            problem at the moment. 
+    */
+    function calcMaxInvestment() private view returns (uint) {
     	// No one invested yet. Put in anything you want
     	if (totalInvestment == 0)
     		return 2^256 - 1;
+
+      // Can't have more that MAX_INVESTORS investors. 
+      require(investorList.length >= MAX_INVESTORS) 
+
 
   		// Some others have invested. Make sure the pool has enough to pay you back. 
   		uint remainingPool = SafeMath.sub(totalInvestment, investments[msg.sender]);
@@ -123,15 +67,21 @@ contract OceanFund is Ownable {
     	return 0;
     }
 
-    // Perhaps add in a limiting feature
+    
+    /* 
+      Investors call this function with the amount of money that they want to put in their pool. 
+      It makes sure the value is enough to pay the BROKER_FEE, takes the commission from the 
+      investment and adds it to the pool. 
+      Also calls calcMaxInvestment to set a hard cap on the pool size. 
+    */
     function invest() public payable {
         require (active);
         
         address investor = msg.sender;
         uint amount = msg.value;
         
-        require(amount >= BROKER_FEE);
-        
+        // Pay broker fee
+        require(amount >= BROKER_FEE);        
         amount = SafeMath.sub(amount, BROKER_FEE);
         
         require (amount <= calcMaxInvestment());
@@ -146,6 +96,10 @@ contract OceanFund is Ownable {
         }
     }
     
+
+    /* 
+      Calculates the total fee that is required to pay all the other investors a POOL_FEE
+    */
     function calculateFee() public view returns (uint) {
         address investor = msg.sender;
         uint remainingPool = totalInvestment - investments[investor];
@@ -153,6 +107,11 @@ contract OceanFund is Ownable {
         return fee;
     }
 
+
+    /* 
+      This function is called privately by extract() to distribute the fees amongst all the 
+      investors when one of them liquidates. 
+    */
     function distributeCommissions(uint fee) private {
         uint moneyLeft = fee;
         
@@ -167,23 +126,49 @@ contract OceanFund is Ownable {
             }
         }
         
+        // Add the total paid out commision to totalInvestment so that it maintains an 
+        // accurate representation of the pool details. 
         totalInvestment = SafeMath.add(totalInvestment, SafeMath.sub(fee, moneyLeft));
+
+        // Since we are doing division, there is a chance there might be excess funds left 
+        // which are unaccounted for. 
         withdrawAmount[broker] = SafeMath.add(withdrawAmount[broker], moneyLeft);
     }
 
-    function extract() public payable {
+
+    /* 
+      Allows an investor to liquidate their funds from their pool investment to withdrawable
+      funds. It takes the distribution fee out of their total principal and sends it out to the 
+      other investors through distributeCommissions. 
+
+      REVERTS: If the investor doesn't have enough funds to pay everybody else. 
+    */
+    function extract() public {
+        require (active);
+        
         address investor = msg.sender;
         uint fee = calculateFee();
-        require(msg.value >= fee);
-        
-        withdrawAmount[investor] = SafeMath.add(SafeMath.sub(msg.value, fee), withdrawAmount[investor]);
+
+        // Calculate the total amount of money investor has in this Pool
         withdrawAmount[investor] = SafeMath.add(investments[investor], withdrawAmount[investor]);
+        // Since this money is extracted, remove it from the pool funds
         totalInvestment = SafeMath.sub(totalInvestment, investments[investor]);
         investments[investor] = 0;
+
+        // Can't take out money if you don't have enough to pay everybody else
+        require(withdrawAmount[investor] >= fee);
         
+        // Remove the fee from the investor's reach 
+        withdrawAmount[investor] = SafeMath.sub(withdrawAmount[investor], fee);
+    
+        // Pay the fee    
         distributeCommissions(fee);
     }
     
+
+    /* 
+        Allows the user to securely withdraw the excess funds that the contract owes them. 
+    */
     function withdraw() public {
         uint amount = withdrawAmount[msg.sender];
         withdrawAmount[msg.sender] = 0;
