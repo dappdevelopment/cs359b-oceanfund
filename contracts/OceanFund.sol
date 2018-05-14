96b5d4d3da78c7a8d5a3e946b5945d856458d8df
Tanay Kothari <tanaykothari1998@gmail.cm>
2018-05-14 12:54:30 -0700

    Modified contract

--- contracts/OceanFund.sol
+++ contracts/OceanFund.sol
@@ -1,6 +1,53 @@
 pragma solidity ^0.4.21;
 
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+
 
 contract OceanFund {
     
@@ -35,7 +82,7 @@ contract OceanFund {
     uint CREATION_FEE = 0.01 ether; 
     uint POOL_DENOMINATION = 10000;
 
-    function OceanFund() public {
+    constructor() public {
         broker = msg.sender;
     }
     
